a3be6467f25ee1bcf4652b1ecb970f3738c90ebc
FedosenkoS <106009943+FedosenkoS@users.noreply.github.com>
2023-07-17 11:15:09 +0300

    Update ex_2.py

--- Lab2/ex_2.py
+++ Lab2/ex_2.py
@@ -1,18 +1,42 @@
-# Задание 2. Задачи на многомерные списки
-#
-# 3.	Даны две квадратных таблицы чисел. Требуется построить третью,
-# каждый элемент которой равен сумме элементов, стоящих на том же месте в 1-й и 2-й таблицах.
-import numpy as np
-n=int(input("Введите размер квадратной таблицы:\n"))
-a = np.random.randint(1, 11, size=(n, n))
-print("Первый список: ")
-print(a)
+def get_distance(p, q):
+    """
+    Return euclidean distance between points p and q
+    assuming both to have the same number of dimensions
+    """
+    # sum of squared difference between coordinates
+    s_sq_difference = 0
+    for p_i, q_i in zip(p, q):
+        s_sq_difference += (p_i - q_i) ** 2
 
-b = np.random.randint(1, 11, size=(n, n))
-print("Второй список: ")
-print(b)
+    # take sq root of sum of squared difference
+    distance = s_sq_difference ** 0.5
+    return distance
 
-c = a + b
 
-print("Третий список (сумма): ")
-print(c)
+# # check the function
+# a = (2, 3, 6)
+# b = (5, 7, 1)
+# # distance b/w a and b
+# d = get_distance(a, b)
+# # display the result
+# print(d)
+
+def distance_in_metr(dist):
+
+    distance = dist * 1000
+
+    return distance
+
+def distance_in_mile(dist):
+
+    distance = dist / 1855
+
+    return distance
+
+#Успеет ли человек преодолеть дистанцию за 8 часов
+def calc_for_men(dist, speed):
+    norm_time = 8
+    time=dist/speed
+    return norm_time>time
+
+
